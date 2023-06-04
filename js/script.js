c73c404bc5f97bae519f45ea89d0f2bd24c2303b
aymanmigal <aymanmigal.q@gmail.com>
2023-06-04 19:49:09 +0100

    MILESTONE 0 AND MILESTONE 1 COMPLETE

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+//MILESTONE 0
+
+const team = [
+
+
+ {
+        name: 'Wayne Barnett',
+        role: 'Founser & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+},
+
+{
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+},
+
+{
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+},
+
+{
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+},
+
+{
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+},
+{
+        name: 'Barbara Ramos',
+        role: 'Graphic Design',
+        image: 'barbara-ramos-graphic-designer.jpg',
+}
+
+]
+
+//MILESTONE 1
+
+for (let i=0; i<team.length; i++){
+
+    console.log(team[i].name);
+    console.log(team[i].role);
+    console.log(team[i].image);
+
+
+
+
+}
